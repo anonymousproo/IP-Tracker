b6e6787ca69db4f879493c6a68ee67d33190ee24
AristodamusAdairs <83016476+AristodamusAdairs@users.noreply.github.com>
2021-06-16 18:52:34 +0500

    Fixed Syntax Errors
    Hope so you see the mistakes! :-)
    Sincerely,
    Hope so you accept the change!

--- IP-Tracker.py
+++ IP-Tracker.py
@@ -27,14 +27,14 @@ print ('''
 		\______|\__|               \__|   \__|  \__|\__|  \__| \______/ \__|  \__|\________|\__|  \__|\033[1;32m
 \033[1;33m
 ''')
-print"\033[1;33m==================================================================================================\033[1;33m"
-print   
-print	"\033[1;32mAuthor			:Mahfuzur Rahman\033[1;32m"
-print	"\033[1;32Github			:https://github.com/anonymousproo\033[1;32m"
-print	"\033[1;32mYouTube			:https://www.youtube.com/anonymouspro1			{IP Tracker v3.0}\033[1;32m"
-print	"\033[1;32mfacebook		:https://m.facebook.com/anonymousproo1\033[1;32m"
-print	"\033[1;32mcoded by		:ANONYMOUS PRO YTB\033[1;32m"
-print
+print("\033[1;33m==================================================================================================\033[1;33m")
+print(   
+print(	"\033[1;32mAuthor			:Mahfuzur Rahman\033[1;32m"                                                   )
+print(	"\033[1;32Github			:https://github.com/anonymousproo\033[1;32m"
+print(	"\033[1;32mYouTube			:https://www.youtube.com/anonymouspro1			{IP Tracker v3.0}\033[1;32m")
+print(	"\033[1;32mfacebook		:https://m.facebook.com/anonymousproo1\033[1;32m"
+print(	"\033[1;32mcoded by		:ANONYMOUS PRO YTB\033[1;32m"
+
 print"\033[1;33m==================================================================================================\033[1;33m"
 print ("\n\033[1;33mYour IP: \033[1;33m" + result1['query'])
 print ("\033[1;32mIf You Do Not Want To Track Type Exit\033[1;32m\n")
@@ -74,23 +74,23 @@ while True:
 
             # printing information
             print ("\n\033[1;33mAll The Information Of IP Is Here \033[1;33m[" + ip + "] :\n")
-            print ("\033[1;33mIP: \033[1;33m" + result['ip'])
-            print ("\033[1;32mIP Type: \033[1;32m" + result['type'])
-            print ("\033[1;34mContinent Name: \033[1;34m" + result['continent_name'])
-            print ("\033[1;34mContinent Code: \033[1;34m" + result['continent_code'])
-            print ("\033[1;33mCountry: \033[1;33m" + result['country_name'])
-            print ("\033[1;33mCountry Code: \033[1;33m" + result1['countryCode'])
-            print ("\033[1;32mRegion Name: \033[1;32m" + result['region_name'])
-            print ("\033[1;32mRegion Code: \033[1;32m" + result['region_code'])
-            print ("\033[1;36mCity: \033[1;36m" + result['city'])
-            print ("\033[1;36mZip: \033[1;36m" + result['zip'])
-            print ("\033[1;33mTimeZone: \033[1;33m" + result1['timezone'])
-            print ("\033[1;33misp: \033[1;33m" + result1['isp'])
-	    print ("Do you want to find the exact location with Google Maps?")
-	    print ("Then search the Google Map using the Latitude or longitude number")
-            print ("\033[1;36mLatitude: \033[1;36m" + lat)
-            print ("\033[1;36mlongitude: \033[1;36m" + long)
-            print ("\033[1;33mMore Information Of IP \033[1;33m:\n" + more)
-            print ("\n\n")
+            print("\033[1;33mIP: \033[1;33m" + result['ip'])
+            print("\033[1;32mIP Type: \033[1;32m" + result['type'])
+            print("\033[1;34mContinent Name: \033[1;34m" + result['continent_name'])
+            print("\033[1;34mContinent Code: \033[1;34m" + result['continent_code'])
+            print("\033[1;33mCountry: \033[1;33m" + result['country_name'])
+            print("\033[1;33mCountry Code: \033[1;33m" + result1['countryCode'])
+            print("\033[1;32mRegion Name: \033[1;32m" + result['region_name'])
+            print("\033[1;32mRegion Code: \033[1;32m" + result['region_code'])
+            print("\033[1;36mCity: \033[1;36m" + result['city'])
+            print("\033[1;36mZip: \033[1;36m" + result['zip'])
+            print("\033[1;33mTimeZone: \033[1;33m" + result1['timezone'])
+            print("\033[1;33misp: \033[1;33m" + result1['isp'])
+	    print("Do you want to find the exact location with Google Maps?")
+	    print("Then search the Google Map using the Latitude or longitude number")
+            print("\033[1;36mLatitude: \033[1;36m" + lat)
+            print("\033[1;36mlongitude: \033[1;36m" + long)
+            print("\033[1;33mMore Information Of IP \033[1;33m:\n" + more)
+            print("\n\n")
         else:
-            print ("\n\033[1;31mSorry, Please Type The IP[" + ip + "] Please try again\033[1;31m\n")
+            print("\n\033[1;31mSorry, Please Type The IP[" + ip + "] Please try again\033[1;31m\n")
